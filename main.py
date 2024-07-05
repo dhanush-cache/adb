72126561d6f4f35a752e74fc3b27e57badbaf4cf
Anonymous <anonymous@example.com>
2024-07-05 09:24:00 +0530

    Add the main program.

---
+++ main.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import adb
+from packages import install, uninstall, disable
+
+# Connect
+if not adb.is_connected():
+    url = input("URL: ")
+    adb.connect(url)
+    if not adb.is_connected():
+        print("Seems like the device isn't paired.")
+        pairing_url = input("Pairing URL: ")
+        code = input("Pairing code: ")
+        adb.pair(pairing_url, code)
+        adb.connect(url)
+
+# Freeze
+for package in disable:
+    if not adb.is_enabled(package):
+        continue
+    print(f"Disabling: {package}...", end=" ")
+    try:
+        adb.clear_data(package)
+        adb.disable(package)
+    except:
+        print("Failed!!!")
+    else:
+        print("Done!")
+
+# Uninstall
+target = Path("/sdcard/sapps")
+target.mkdir(parents=True, exist_ok=True)
+for package in uninstall:
+    if not adb.is_installed(package):
+        continue
+    print(f"Uninstalling: {package}...", end=" ")
+    try:
+        adb.clear_data(package)
+        adb.extract(package, target)
+        adb.uninstall(package)
+    except:
+        print("Failed!!!")
+    else:
+        print("Done")
